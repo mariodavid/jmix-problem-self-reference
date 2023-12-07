03a04499be24a2bb8f2cb2e42b660c20a673342f
Mario David <mario@die-davids.de>
2023-12-07 13:31:25 +0100

    add simplest example

--- README.md
+++ README.md
@@ -9,17 +9,84 @@ In the DB we have the relationships:
 
 ![](/img/db-content.png)
 
-### Tree Data Grid
-In the data grid it is correctly displaying the tree structure of the employees.
 
-![](/img/tree-data-grid.png)
+## Problem
 
-### Virtual List
+When having a data model that is self-referencing (like Employee has a Manager, which is also an employee in this example),
+lazy loading does not work when accessing the data through the `@OneToOne` inverse association.
 
-The virtual list is supposed to be doing the following:
+### Domain Model
 
-The data container loads only the top level rows (manager = null), but with a fetch plan,
-to include the report (transient 1:1 inverse relationship of `manager`).
+```java
+@JmixEntity
+@Table(name = "EMPLOYEE", indexes = {
+        @Index(name = "IDX_EMPLOYEE_MANAGER", columnList = "MANAGER_ID")
+})
+@Entity
+public class Employee {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MANAGER_ID")
+    private Employee manager;
 
 
-![](/img/virtual-list.png)
+    @OneToOne(mappedBy = "manager", fetch = FetchType.LAZY)
+    private Employee report;
+}
+```
+
+### Test
+
+Given the DB contains the above-mentioned data: 1 -> 2 -> 3, when loading through data manager without specifying a fetch plan that includes the `report` attribute, the lazy loading behaviour is wrong. Instead of `1 -> 2 -> 3` it returns `1 -> 2 -> 1`.
+
+See the following test [EmployeeTest.java](/src/test/java/com/company/jpsr/EmployeeTest.java):
+
+```java
+public class EmployeeTest {
+    /**
+     * this test fails, but it should pass. It is not specifying the loading mechanism upfront and instead relies
+     * on lazy loading. Due to this, the chain is not correctly specified. Instead of the actual chain:
+     * 1 -> 2 -> 3, the objects represent the following chain: 1 -> 2 -> 1 - which is incorrect
+     */
+    @Test
+    void given_fetchPlanContainsManager_andNotReport_expect_chainIsCorrect() {
+
+        // given:
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList("SELECT id, manager_id FROM EMPLOYEE ORDER BY id");
+
+        // expect: employee 1 has no manager in DB
+        assertThat(rows.get(0).get("id")).isEqualTo(uuid(1));
+        assertThat(rows.get(0).get("manager_id")).isNull();
+
+        // and: employee 2 has manager 1 in DB
+        assertThat(rows.get(1).get("id")).isEqualTo(uuid(2));
+        assertThat(rows.get(1).get("manager_id")).isEqualTo(uuid(1));
+
+        // and: employee 3 has manager 2 in DB
+        assertThat(rows.get(2).get("id")).isEqualTo(uuid(3));
+        assertThat(rows.get(2).get("manager_id")).isEqualTo(uuid(2));
+
+        // when:
+        Employee loadedEmployee1 =
+                dataManager.load(Employee.class)
+                        .query("select e from Employee e where e.manager is null")
+                        .fetchPlan(fetchPlanBuilder ->
+                                // we explicitly don't automatically fetch the "report" attribute.
+                                // this triggers the behaviour that the lazy loading through multiple levels
+                                // does not work anymore
+                                fetchPlanBuilder.addFetchPlan(FetchPlan.LOCAL)
+                        )
+                        .list().stream().findFirst().orElseThrow();
+
+        // then: the first report is correct
+        assertThat(loadedEmployee1.getReport()).isEqualTo(employee2);
+
+        // but: the second report is incorrect. It says it is "employee1" instead - which describes the bug
+        assertThat(loadedEmployee1.getReport().getReport()).isEqualTo(employee3);
+    }
+}
+```
--- src/test/java/com/company/jpsr/EmployeeTest.java
+++ src/test/java/com/company/jpsr/EmployeeTest.java
@@ -5,20 +5,20 @@ import com.company.jpsr.test_support.AuthenticatedAsAdmin;
 import io.jmix.core.DataManager;
 import io.jmix.core.EntityStates;
 import io.jmix.core.FetchPlan;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
- * Sample integration test for the User entity.
- */
 @SpringBootTest
 @ExtendWith(AuthenticatedAsAdmin.class)
 public class EmployeeTest {
@@ -29,43 +29,117 @@ public class EmployeeTest {
     @Autowired
     EntityStates entityStates;
 
-    @Test
-    void given_employeeManagerChainOfThree_when_loadTopLevelManagersWithFirstLevelReports_expect_() {
+    @Autowired
+    DataSource dataSource;
+    private Employee employee1;
+    private Employee employee2;
+    private Employee employee3;
+    private JdbcTemplate jdbcTemplate;
 
-        // given:
-        Employee employee1 = saveEmployee("1", null);
-        Employee employee2 = saveEmployee("2", employee1);
-        Employee employee3 = saveEmployee("3", employee2);
+    @BeforeEach
+    void setupData() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.update("DELETE FROM EMPLOYEE");
 
-        // when:
-        List<Employee> employeesWithoutManagers =
-                dataManager.load(Employee.class).query("select e from Employee e where e.manager is null")
-                        .fetchPlan(fetchPlanBuilder -> fetchPlanBuilder.add("report", FetchPlan.BASE))
-                        .list();
-
-        // then:
-        assertThat(employeesWithoutManagers).hasSize(1);
-        Employee loadedEmployee1 = employeesWithoutManagers.get(0);
-
-        // and:
-        assertThat(entityStates.isLoaded(loadedEmployee1, "report")).isTrue();
-        assertThat(loadedEmployee1.getReport().getName()).isEqualTo("2");
-
-        // and:
-        assertThat(entityStates.isLoaded(loadedEmployee1.getReport(), "report")).isFalse();
-        assertThat(loadedEmployee1.getReport().getReport().getName()).isEqualTo("3");
+        employee1 = saveEmployee(1, null);
+        employee2 = saveEmployee(2, employee1);
+        employee3 = saveEmployee(3, employee2);
     }
 
-    private Employee saveEmployee(String name, Employee manager) {
+    /**
+     * this test is working, and it is also supposed to be working
+     */
+    @Test
+    void given_fetchPlanContainsReport_andNotManager_expect_chainIsCorrect() {
+
+        // given:
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList("SELECT id, manager_id FROM EMPLOYEE ORDER BY id");
+
+        // expect: employee 1 has no manager in DB
+        assertThat(rows.get(0).get("id")).isEqualTo(uuid(1));
+        assertThat(rows.get(0).get("manager_id")).isNull();
+
+        // and: employee 2 has manager 1 in DB
+        assertThat(rows.get(1).get("id")).isEqualTo(uuid(2));
+        assertThat(rows.get(1).get("manager_id")).isEqualTo(uuid(1));
+
+        // and: employee 3 has manager 2 in DB
+        assertThat(rows.get(2).get("id")).isEqualTo(uuid(3));
+        assertThat(rows.get(2).get("manager_id")).isEqualTo(uuid(2));
+
+        // when:
+        Employee loadedEmployee1 =
+                dataManager.load(Employee.class)
+                        .query("select e from Employee e where e.manager is null")
+                        .fetchPlan(fetchPlanBuilder -> {
+
+                                // here were are adding the "report" attribute to load directly the corresponding child elements
+                                fetchPlanBuilder.add("report", FetchPlan.BASE);
+                                fetchPlanBuilder.add("report.report", FetchPlan.BASE);
+                        })
+                        .list().stream().findFirst().orElseThrow();
+
+        // then:
+        assertThat(loadedEmployee1.getReport()).isEqualTo(employee2);
+
+        // and:
+        assertThat(loadedEmployee1.getReport().getReport()).isEqualTo(employee3);
+    }
+
+
+    /**
+     * this test fails, but it should pass. It is not specifying the loading mechanism upfront and instead relies
+     * on lazy loading. Due to this, the chain is not correctly specified. Instead of the actual chain:
+     * 1 -> 2 -> 3, the objects represent the following chain: 1 -> 2 -> 1 - which is incorrect
+     */
+    @Test
+    void given_fetchPlanContainsManager_andNotReport_expect_chainIsCorrect() {
+
+        // given:
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList("SELECT id, manager_id FROM EMPLOYEE ORDER BY id");
+
+        // expect: employee 1 has no manager in DB
+        assertThat(rows.get(0).get("id")).isEqualTo(uuid(1));
+        assertThat(rows.get(0).get("manager_id")).isNull();
+
+        // and: employee 2 has manager 1 in DB
+        assertThat(rows.get(1).get("id")).isEqualTo(uuid(2));
+        assertThat(rows.get(1).get("manager_id")).isEqualTo(uuid(1));
+
+        // and: employee 3 has manager 2 in DB
+        assertThat(rows.get(2).get("id")).isEqualTo(uuid(3));
+        assertThat(rows.get(2).get("manager_id")).isEqualTo(uuid(2));
+
+        // when:
+        Employee loadedEmployee1 =
+                dataManager.load(Employee.class)
+                        .query("select e from Employee e where e.manager is null")
+                        .fetchPlan(fetchPlanBuilder ->
+                                // we explicitly don't automatically fetch the "report" attribute.
+                                // this triggers the behaviour that the lazy loading through multiple levels
+                                // does not work anymore
+                                fetchPlanBuilder.addFetchPlan(FetchPlan.LOCAL)
+                        )
+                        .list().stream().findFirst().orElseThrow();
+
+        // then: the first report is correct
+        assertThat(loadedEmployee1.getReport()).isEqualTo(employee2);
+
+        // but: the second report is incorrect. It says it is "employee1" instead - which describes the bug
+        assertThat(loadedEmployee1.getReport().getReport()).isEqualTo(employee3);
+    }
+
+    private Employee saveEmployee(int number, Employee manager) {
         Employee employee = dataManager.create(Employee.class);
-        employee.setName(name);
+        employee.setId(uuid(number));
+        employee.setName("" + number);
         employee.setManager(manager);
         dataManager.save(employee);
         return employee;
     }
 
-    @AfterEach
-    void tearDown() {
-        dataManager.load(Employee.class).all().list().forEach(dataManager::remove);
+    private static UUID uuid(int number) {
+        return UUID.fromString("00000000-0000-0000-0000-00000000000" + number);
     }
+
 }
